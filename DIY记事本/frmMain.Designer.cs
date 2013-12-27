30e9fa7a903aea6ec663843552632a66c17e149a
HuoDa <huoda2008@163.com>
2013-12-27 20:21:16 +0800

    commit all

---
+++ DIY记事本/frmMain.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace DIY记事本
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.编辑ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.全选ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.复制ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zhantieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 26);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(563, 427);
+            this.textBox1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.编辑ToolStripMenuItem,
+            this.帮助ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(576, 25);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.打开ToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 打开ToolStripMenuItem
+            // 
+            this.打开ToolStripMenuItem.Name = "打开ToolStripMenuItem";
+            this.打开ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.打开ToolStripMenuItem.Text = "打开";
+            // 
+            // 编辑ToolStripMenuItem
+            // 
+            this.编辑ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.全选ToolStripMenuItem,
+            this.复制ToolStripMenuItem,
+            this.zhantieToolStripMenuItem});
+            this.编辑ToolStripMenuItem.Name = "编辑ToolStripMenuItem";
+            this.编辑ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.编辑ToolStripMenuItem.Text = "编辑";
+            // 
+            // 全选ToolStripMenuItem
+            // 
+            this.全选ToolStripMenuItem.Name = "全选ToolStripMenuItem";
+            this.全选ToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.全选ToolStripMenuItem.Text = "全选";
+            // 
+            // 复制ToolStripMenuItem
+            // 
+            this.复制ToolStripMenuItem.Name = "复制ToolStripMenuItem";
+            this.复制ToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.复制ToolStripMenuItem.Text = "复制";
+            // 
+            // zhantieToolStripMenuItem
+            // 
+            this.zhantieToolStripMenuItem.Name = "zhantieToolStripMenuItem";
+            this.zhantieToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.zhantieToolStripMenuItem.Text = "zhantie";
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.帮助ToolStripMenuItem.Text = "帮助";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 465);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.Text = "记事本";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 编辑ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 打开ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 全选ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 复制ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zhantieToolStripMenuItem;
+    }
+}
+
---
+++ DIY记事本/frmMain.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
+using ExtendNotePad;
+
+namespace DIY记事本
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            //在窗体加载的时候搜索addins目录下的所有的dll文件
+            //Assembly.GetExecutingAssembly() 获取当前正在运行的exe文件
+            //Assembly.GetExecutingAssembly().Location 获得该exe文件的完整路径
+            string dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            //将两个路径连接起来
+            string addinsPath = Path.Combine(dir, "addins");
+
+          //搜索指定目录下所有的dll文件
+            string[] dlls = Directory.GetFiles(addinsPath, "*.dll");
+
+            //循环加载所有的dll文件
+            for (int i = 0; i < dlls.Length; i++)
+            {
+                //将每一个dll文件都加载进来
+                Assembly ass = Assembly.LoadFile(dlls[i]);
+
+                //获取当前dll（插件）中定义的所有public类型
+                Type[] typs = ass.GetExportedTypes();
+
+                //获得IEditor的Type
+                Type typIEditor = typeof(IEditor);
+
+                //循环判断每一个类型（Type）是否实现了IEditor接口
+                for (int j = 0; j < typs.Length; j++)
+                {
+                    //typIEditor.IsAssignableFrom(typs[j])
+                    //判断程序集中的类型是否能赋值给IEditor的Type
+                    //如果能，则表示该类型实现了IEditor接口
+
+                    //这里其实需要做两个判断
+                    //1.该类型实现了IEditor接口
+                    //2.该类型可以实例化
+                    if (typIEditor.IsAssignableFrom(typs[j]) && !typs[j].IsAbstract)
+                    {
+                        //根据类型的Type，创建类型的对象
+                        //由于已经明确了typs[j]肯定是实现了IEditor接口的类型
+                        //所以可以将该类型的对象 显示转换为IEditor类型，然后通过接口变量操作类的成员
+                        IEditor editor = (IEditor)Activator.CreateInstance(typs[j]);
+                        editor.Excute();
+                    }
+                }
+
+            }
+            
+            //获取dll文件中的具有插件功能的类
+            //通过调用插件中的类的方法实现对记事本的功能的扩展
+            //开发主程序的人必须做一个约定：所有为该程序开发插件的人必须将插件的执行方法命名为Excute()
+            //主程序的开发人员不管插件开发者定义了多个类、方法，在主程序中只去找Excute（）方法来调用。这就是主程序的开发者与插件开发者的一个约定
+            //接口就是一个约定，这个接口中有一个名字叫Excute()方法
+
+        }
+    }
+}
